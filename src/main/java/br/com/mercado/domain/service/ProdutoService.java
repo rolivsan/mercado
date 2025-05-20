39f42cead63801f230214f6191037790c03a6f46
ELAINE PAZ <elaine.apaz@telefonicabigdata.onmicrosoft.com>
2025-05-19 23:22:56 -0300

    Adiciona metodo create

--- src/main/java/br/com/mercado/domain/service/ProdutoService.java
+++ src/main/java/br/com/mercado/domain/service/ProdutoService.java
@@ -1,8 +1,9 @@
 package br.com.mercado.domain.service;
 
 import br.com.mercado.domain.dto.request.ProdutoRequest;
+import br.com.mercado.domain.dto.response.ProdutoResponse;
 import br.com.mercado.domain.model.Produto;
-import br.com.mercado.domain.useCase.PrdutoUseCase;
+import br.com.mercado.domain.useCase.ProdutoUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,12 @@ import java.util.List;
 public class ProdutoService {
 
     @Autowired
-    PrdutoUseCase produtoUseCase;
+    ProdutoUseCase produtoUseCase;
 
     public List<Produto> getAll() {
         return produtoUseCase.getAll();
     }
-    public Produto create(ProdutoRequest produtoRequest) throws Exception {
+    public ProdutoResponse create(ProdutoRequest produtoRequest) throws Exception {
         return produtoUseCase.create(produtoRequest);
     }
 }
--- src/main/java/br/com/mercado/domain/useCase/ProdutoUseCase.java
+++ src/main/java/br/com/mercado/domain/useCase/ProdutoUseCase.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 @Component
-public class PrdutoUseCase {
+public class ProdutoUseCase {
 
     @Autowired
     ProdutoRepository produtoRepository;
